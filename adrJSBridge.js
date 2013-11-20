e44e338f7dd86fe65a6364ac78268434655aa5c0
Scott Olson <scottyolson@gmail.com>
2013-11-20 14:17:20 -0600

    integrating the module pattern

--- adrJSBridge.js
+++ adrJSBridge.js
@@ -1,121 +1,104 @@
-function sendEmail(to, cc, subject, body, successCallback, errorCallback)
-{
-	if(useAPI)
-	{
-		API.sendEmail();
-	}
-	else
-		_callNativeFunction("sendEmail", [to, cc, subject, body], successCallback, errorCallback);
-}
+var adr = function() {
 
+    var _callNativeFunction = function(functionName, args, successCallback, errorCallback) {
+        var url = "adrJSBridge://";
 
-function getItem(key, successCallback, errorCallback)
-{
-    if(useAPI)
-    {
-        API.getItem(key);
-    }
-    else
-        _callNativeFunction("getValue", [key], successCallback, errorCallback);
-}
+        var callInfo = {};
+        callInfo.functionname = functionName;
 
-function setItem(key, value, successCallback, errorCallback)
-{
-    if(useAPI)
-    {
-        API.setItem(key, value);
-    }
-    else
-        _callNativeFunction("setValue", [key, value], successCallback, errorCallback);
-}
-
-function logEvent(object, action, additionalParams, successCallback, errorCallback)
-{
-    if(useAPI)
-    {
-        API.logEvent(object, action, additionalParams);
-    }
-    else
-        _callNativeFunction("logEvent", [object, action, additionalParams], successCallback, errorCallback);
-}
-
-function _callNativeFunction(functionName, args, successCallback, errorCallback)
-{
-    var url = "adrJSBridge://";
-
-    var callInfo = {};
-    callInfo.functionname = functionName;
-
-    if (successCallback)
-    {
-        if (typeof successCallback == 'function')
+        if (successCallback)
         {
-            var callbackFuncName = _createCallbackFunction(functionName + "_" + "successCallback", successCallback);
-            callInfo.success = callbackFuncName;
-        }
-        else
-            callInfo.success = successCallback;
-    }
-
-    if (errorCallback)
-    {
-        if (typeof errorCallback == 'function')
-        {
-            var callbackFuncName = _createCallbackFunction(functionName + "_" + "errorCallback", errorCallback);
-            callInfo.error = callbackFuncName;
-        }
-        else
-            callInfo.error = errorCallback;
-    }
-
-    if (args)
-    {
-        callInfo.args = args;
-    }
-
-    url += JSON.stringify(callInfo)
-
-
-    var iFrame = _createIFrame(url);
-    //remove the frame now
-    iFrame.parentNode.removeChild(iFrame);
-}
-
-function _createCallbackFunction (funcName, callbackFunc)
-{
-    if (callbackFunc && callbackFunc.name != null && callbackFunc.name.length > 0)
-    {
-        return callbackFunc.name;
-    }
-
-    if (typeof window[funcName+0] != 'function')
-    {
-        window[funcName+0] = callbackFunc;
-        __functionIndexMap[funcName] = 0;
-        return funcName+0
-
-    } else
-    {
-        var maxIndex = __functionIndexMap[funcName];
-        var callbackFuncStr = callbackFunc.toString();
-        for (var i = 0; i <= maxIndex; i++)
-        {
-            var tmpName = funcName + i;
-            if (window[tmpName].toString() == callbackFuncStr)
-                return tmpName;
+            if (typeof successCallback == 'function')
+            {
+                var callbackFuncName = _createCallbackFunction(functionName + "_" + "successCallback", successCallback);
+                callInfo.success = callbackFuncName;
+            }
+            else
+                callInfo.success = successCallback;
         }
 
-        var newIndex = ++__functionIndexMap[funcName];
-        window[funcName+newIndex] = callbackFunc;
-        return funcName+newIndex;
-    }
-}
+        if (errorCallback)
+        {
+            if (typeof errorCallback == 'function')
+            {
+                var callbackFuncName = _createCallbackFunction(functionName + "_" + "errorCallback", errorCallback);
+                callInfo.error = callbackFuncName;
+            }
+            else
+                callInfo.error = errorCallback;
+        }
 
-function _createIFrame(src)
-{
-    var rootElm = document.documentElement;
-    var newFrameElm = document.createElement("IFRAME");
-    newFrameElm.setAttribute("src",src);
-    rootElm.appendChild(newFrameElm);
-    return newFrameElm;
-}
+        if (args)
+        {
+            callInfo.args = args;
+        }
+
+        url += JSON.stringify(callInfo)
+
+
+        var iFrame = _createIFrame(url);
+        //remove the frame now
+        iFrame.parentNode.removeChild(iFrame);
+    };
+    var _createCallbackFunction = function(funcName, callbackFunc) {
+        if (callbackFunc && callbackFunc.name != null && callbackFunc.name.length > 0)
+        {
+            return callbackFunc.name;
+        }
+
+        if (typeof window[funcName+0] != 'function')
+        {
+            window[funcName+0] = callbackFunc;
+            __functionIndexMap[funcName] = 0;
+            return funcName+0
+
+        } else
+        {
+            var maxIndex = __functionIndexMap[funcName];
+            var callbackFuncStr = callbackFunc.toString();
+            for (var i = 0; i <= maxIndex; i++)
+            {
+                var tmpName = funcName + i;
+                if (window[tmpName].toString() == callbackFuncStr)
+                    return tmpName;
+            }
+
+            var newIndex = ++__functionIndexMap[funcName];
+            window[funcName+newIndex] = callbackFunc;
+            return funcName+newIndex;
+        }
+    };
+    var _createIFrame = function(src) {
+        var rootElm = document.documentElement;
+        var newFrameElm = document.createElement("IFRAME");
+        newFrameElm.setAttribute("src",src);
+        rootElm.appendChild(newFrameElm);
+        return newFrameElm;
+    };
+    return {
+        sendEmail : function(to, cc, subject, body, successCallback, errorCallback) {
+            if(typeof useAPI === 'undefined')
+                _callNativeFunction("sendEmail", [to, cc, subject, body], successCallback, errorCallback);
+            else
+                API.sendEmail();
+        },
+        getItem : function(key, successCallback, errorCallback) {
+            if(typeof useAPI === 'undefined')
+                _callNativeFunction("getValue", [key], successCallback, errorCallback);
+            else
+                API.getItem(key);
+        },
+        setItem : function(key, value, successCallback, errorCallback) {
+            if(typeof useAPI === 'undefined')
+                _callNativeFunction("setValue", [key, value], successCallback, errorCallback);
+            else
+                API.setItem(key, value);
+        },
+        logEvent : function(object, action, additionalParams, successCallback, errorCallback) {
+            if(typeof useAPI === 'undefined')
+                _callNativeFunction("logEvent", [object, action, additionalParams], successCallback, errorCallback);
+            else
+                API.logEvent(object, action, additionalParams);
+        }
+    };
+}();
